52a74335a853519e7e3738c9fe8297fe0c54d28d
Amanda Norris <amandakathleennorris@gmail.com>
2023-11-20 15:19:21 -0600

    created inquirer prompts

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+// requiring inquirer
+const inquirer = require('inquirer');
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+//inquirer prompts user for input
+function init() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Please enter a description of the project',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Please enter installation instructions',
+            name: 'install'
+        },
+
+        {
+            type: 'input',
+            message: 'Please enter usage instructions.',
+            name: 'usage'
+        },
+        {
+            type: 'list',
+            message: 'What license is your project using?',
+            name: 'license',
+            choices: []
+        },
+        {
+            type: 'input',
+            message: 'How can others contribute to your project?',
+            name: 'contribute'
+        },
+        {
+            type: 'input',
+            message: 'Please input the tests for your project',
+            name: 'tests'
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub url?',
+            name: 'github'
+        },
+    ])
+}
 
 // Function call to initialize app
 init();
